6640bbce69f150703d1a1fa0634faf3f565f7601
RJPG <regis.garnier@wanadoo.fr>
2024-09-29 23:57:24 +0200

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -27,7 +27,7 @@ def count():
     if data:
         return jsonify(length=len(data)), 200
 
-    return {"message": "Internal server error"}, 500
+    return {"Message": "Internal server error"}, 500
 
 
 ######################################################################
@@ -35,7 +35,11 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    """return all data"""
+    if data:
+        return jsonify(data), 200
+
+    return {"Message": "Internal server error"}, 500
 
 ######################################################################
 # GET A PICTURE
@@ -44,15 +48,36 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
-
-
+    """return a data when id"""
+    if data:
+        picture_found = None
+        for picture in data:
+            if int(picture["id"]) == int(id):
+                picture_found = picture
+                break
+        if  picture_found:                     
+            return jsonify(picture_found), 200
+        return {"Message": "picture not found"}, 404   
+    return {"Message": "Internal server error"}, 500    
+    
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    """create a picture from JSON POST"""
+    data_req = request.json
+    if data:
+        picture_found = None
+        for picture in data:
+            if int(picture["id"]) == int(data_req["id"]):
+                picture_found = picture
+                break
+        if  picture_found:                     
+            return {"Message": f"picture with id {picture['id']} already present"}, 302 
+        data.append(data_req)
+        return jsonify(data_req), 201   
+    return {"Message": "Internal server error"}, 500      
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +86,38 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    """update a picture from JSON PUT"""
+    data_req = request.json
+    if data:
+        picture_found = None
+        for picture in data:
+            if int(picture["id"]) == int(data_req["id"]):
+                picture["pic_url"]=data_req["pic_url"]
+                picture["event_country"]=data_req["event_country"]
+                picture["event_state"]=data_req["event_state"]
+                picture["event_city"]=data_req["event_city"]
+                picture["event_date"]=data_req["event_date"]
+                picture_found = picture
+                break
+        if  picture_found:                     
+            return jsonify(picture_found), 200
+        return {"Message": "picture not found"}, 404  
+    return {"Message": "Internal server error"}, 500   
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    """delete a picture from JSON DELETE"""
+    if data:
+        picture_found = None
+        for picture in data:
+            if int(picture["id"]) == int(id):
+                picture_found = picture
+                break
+        if  picture_found:
+            data.remove(picture_found)                     
+            return jsonify(picture_found), 204
+        return {"Message": "picture not found"}, 404   
+    return {"Message": "Internal server error"}, 500   
